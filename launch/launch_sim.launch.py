c76de3b7a0da77a97a96b6e46a69abffd08aaf49
T-bonesLek <torbjorn.s.halvorsen@gmail.com>
2024-11-01 17:56:40 +0100

    working and drivable robot in a play room of different obstacles to avoid

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,47 +1,59 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
+    package_name = 'tbot_helper'
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='tbot_helper'
-
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    # Set the GAZEBO_MODEL_PATH environment variable
+    gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=os.path.join(get_package_share_directory(package_name), 'models')
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Include the RSP launch file
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Load the world in Gazebo
+    world_file = os.path.join(get_package_share_directory(package_name), 'worlds', 'simple.world')
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'world': world_file}.items()
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'tbot_helper'],
-                        output='screen')
+    # Spawn the entity in Gazebo
+    spawn_entity = Node(
+        package='gazebo_ros', executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'tbot_helper'],
+        output='screen'
+    )
 
+    # Open a new terminal for RViz
+    rviz = ExecuteProcess(
+        cmd=['terminator', '-e', 'bash -c "source /opt/ros/humble/setup.bash && rviz2 -d .rviz2/config/devtbot.rviz"'],
+        output='screen'
+    )
 
+    # Open a new terminal for teleop twist
+    teleop_twist = ExecuteProcess(
+        cmd=['terminator', '-e', 'bash -c "source /opt/ros/humble/setup.bash && ros2 run teleop_twist_keyboard teleop_twist_keyboard"'],
+        output='screen'
+    )
 
     # Launch them all!
     return LaunchDescription([
+        gazebo_model_path,  # Include the environment variable
         rsp,
         gazebo,
         spawn_entity,
+        # rviz,
+        teleop_twist,
     ])
